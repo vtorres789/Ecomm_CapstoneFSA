f15d89c41a163acf8dbf213c910aeb97fd309640
vtorres789 <victortorres180@gmail.com>
2023-08-31 22:13:35 -0400

    API introduced with useEffect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+ 
+    useEffect(() => {
+      fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+    }, []);
+
     return (
       <div className="home">
         <h1>World Soccer MegaShop Plus</h1>
